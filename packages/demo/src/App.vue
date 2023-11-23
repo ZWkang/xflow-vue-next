188604a9e67661281672382615b6a00538b3da4f
zwkang <kang95630@gmail.com>
2023-11-23 11:47:59 +0800

    feat: add demo

---
+++ packages/demo/src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { XFlow, XFlowGraph, Background } from 'xflow-vue-next';
+
+const bgColor = ref('red');
+</script>
+
+<template>
+  <div class="container">
+    <XFlow>
+      <Background :color="bgColor" />
+      <XFlowGraph
+        zoomable
+        pannable
+        centerView
+        fitView
+        :connectionEdgeOptions="{
+          attrs: {
+            line: {
+              stroke: '#8f8f8f',
+              strokeWidth: 1,
+            },
+          },
+        }"
+      />
+    </XFlow>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 1200px;
+  height: 600px;
+  margin-bottom: 50px;
+  border-radius: 5px;
+}
+</style>
